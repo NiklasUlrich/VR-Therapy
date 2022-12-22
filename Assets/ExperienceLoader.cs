38681e9f801806985db7b964c6402382a19da465
Snifflaz <eisfreiesspeiseeis@gmail.com>
2022-12-22 12:49:41 +0100

    Implemented Scene change by putting sphere on head

--- Assets/ExperienceLoader.cs
+++ Assets/ExperienceLoader.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceLocations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
 
 public class ExperienceLoader : MonoBehaviour
 {
+    public string key;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,27 @@ public class ExperienceLoader : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "MainCamera")
+        {
+            LoadScene();
+        }
+        Debug.Log("Collision detected");
+    }
+
+    private void LoadScene()
+    {
+        Addressables.LoadSceneAsync(key, LoadSceneMode.Single).Completed += SceneLoadComplete;
+    }
+
+    private void SceneLoadComplete(AsyncOperationHandle<SceneInstance> obj)
+    {
+        if (obj.Status == AsyncOperationStatus.Succeeded)
+        {
+            Debug.Log(obj.Result.Scene.name + " successfully loaded");
+        }
     }
 }
