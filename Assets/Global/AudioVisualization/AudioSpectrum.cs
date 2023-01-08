08269343ec78354c5c1ecf395c276150a1fdc0ab
Snifflaz <eisfreiesspeiseeis@gmail.com>
2023-01-08 16:27:44 +0100

    Testing AudioSpectrum
    Ok, so this shit does not work with wwise?

--- Assets/Global/AudioVisualization/AudioSpectrum.cs
+++ Assets/Global/AudioVisualization/AudioSpectrum.cs
@@ -20,6 +20,7 @@ public class AudioSpectrum : MonoBehaviour
         if (m_audioSpectrum != null && m_audioSpectrum.Length > 0)
         {
             spectrumValue = m_audioSpectrum[0] * 100;
+            Debug.Log(spectrumValue);
         }
     }
 
