b7dc4fb619444c8b14c76cec8ca167c80a3b7367
Snifflaz <eisfreiesspeiseeis@gmail.com>
2023-08-06 21:33:15 +0200

    Revert "mistakes"
    This reverts commit 615cf1e532a657918764a86c914529176c8acf3d.

--- Assets/Global/PermanentObject.cs
+++ Assets/Global/PermanentObject.cs
@@ -7,9 +7,15 @@ public class PermanentObject : MonoBehaviour
     public static PermanentObject me;
     private void Awake()
     {
-
+        if (!me)
+        {
+            me = this;
             DontDestroyOnLoad(gameObject);
-
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
     }
     // Start is called before the first frame update
     void Start()
